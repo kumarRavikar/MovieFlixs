6c925e12fb1f8302c70c7fffd3c6cef9dec3a152
kumar ravikar <kumarravikar24@gmail.com>
2025-12-11 00:41:31 +0530

    Adding search functionality

--- src/App.js
+++ src/App.js
@@ -1,73 +1,137 @@
-import  { Component } from "react";
+// import  { Component } from "react";
+// import MovieList from "./MovieList";
+// import Navbar from "./navBar";
+// import { movies } from "./moviesData";
+
+// class App extends Component  {
+//   constructor(){
+//         super();
+//         this.state = {
+//            movies : movies,
+//            cartCount : 0
+//         }
+//     }
+  
+//     handilIncStar=(movie)=>{
+//        const {movies} = this.state
+//        const mId = movies.indexOf(movie);
+//        if(movies[mId].stars >= 5) return;
+//        movies[mId].stars += 0.5;
+//        this.setState({
+//         movies:movies
+//        })
+//     }
+//     handilDecStar = (movie)=>{
+//       const {movies} = this.state;
+//       const mId = movies.indexOf(movie);
+//       if(movies[mId].stars <= 0)return;
+//       movies[mId].stars -= 0.5;
+//       this.setState({
+//         movies
+//       })
+//     }
+//     handileFev =(movie)=>{
+//       const {movies} = this.state
+//       const mId = movies.indexOf(movie)
+//       movies[mId].fav = !movies[mId].fav
+//         this.setState({
+//             movies
+//         })
+//     }
+//     handilCart =(movie)=>{
+//         let {movies,cartCount} = this.state
+//         const mId = movies.indexOf(movie)
+//         movies[mId].cart = !movies[mId].cart
+//         if(movies[mId].cart){
+//           cartCount += 1;
+//         }else{
+//           cartCount -= 1;
+//         }
+//         this.setState({
+//             movies,
+//             cartCount
+//         })
+//         console.log(cartCount)
+//     }
+  
+//   render(){
+//     const {movies, cartCount} = this.state
+//     return (
+//    <>
+//    <Navbar cartCount={cartCount}/>
+//    <MovieList movies ={movies}
+//               addStars ={this.handilIncStar}
+//               decStars ={this.handilDecStar}
+//               addFav={this.handileFev}
+//               addCart ={this.handilCart} />
+//    </>
+//   );
+//   }
+  
+// }
+
+// export default App;
+import { useState } from "react";
 import MovieList from "./MovieList";
 import Navbar from "./navBar";
-import { movies } from "./moviesData";
+import { movies as moviesData } from "./moviesData";
 
-class App extends Component  {
-  constructor(){
-        super();
-        this.state = {
-           movies : movies,
-           cartCount : 0
-        }
-    }
-  
-    handilIncStar=(movie)=>{
-       const {movies} = this.state
-       const mId = movies.indexOf(movie);
-       if(movies[mId].stars >= 5) return;
-       movies[mId].stars += 0.5;
-       this.setState({
-        movies:movies
-       })
-    }
-    handilDecStar = (movie)=>{
-      const {movies} = this.state;
-      const mId = movies.indexOf(movie);
-      if(movies[mId].stars <= 0)return;
-      movies[mId].stars -= 0.5;
-      this.setState({
-        movies
-      })
-    }
-    handileFev =(movie)=>{
-      const {movies} = this.state
-      const mId = movies.indexOf(movie)
-      movies[mId].fav = !movies[mId].fav
-        this.setState({
-            movies
-        })
-    }
-    handilCart =(movie)=>{
-        let {movies,cartCount} = this.state
-        const mId = movies.indexOf(movie)
-        movies[mId].cart = !movies[mId].cart
-        if(movies[mId].cart){
-          cartCount += 1;
-        }else{
-          cartCount -= 1;
-        }
-        this.setState({
-            movies,
-            cartCount
-        })
-        console.log(cartCount)
-    }
-  
-  render(){
-    const {movies, cartCount} = this.state
-    return (
-   <>
-   <Navbar cartCount={cartCount}/>
-   <MovieList movies ={movies}
-              addStars ={this.handilIncStar}
-              decStars ={this.handilDecStar}
-              addFav={this.handileFev}
-              addCart ={this.handilCart} />
-   </>
+const App = () => {
+
+  const [movies, setMovies] = useState(moviesData);
+  const [cartCount, setCartCount] = useState(0);
+  const [search, setSearch] = useState("");
+  //  Increase stars
+  const handleIncStar = (movie) => {
+    const updated = movies.map((m) =>
+      m === movie && m.stars < 5 ? { ...m, stars: m.stars + 0.5 } : m
+    );
+    setMovies(updated);
+  };
+
+  //  Decrease stars
+  const handleDecStar = (movie) => {
+    const updated = movies.map((m) =>
+      m === movie && m.stars > 0 ? { ...m, stars: m.stars - 0.5 } : m
+    );
+    setMovies(updated);
+  };
+
+  //Toggle Favourite
+  const handleFav = (movie) => {
+    const updated = movies.map((m) =>
+      m === movie ? { ...m, fav: !m.fav } : m
+    );
+    setMovies(updated);
+  };
+
+  // 🛒 Add/Remove Cart
+  const handleCart = (movie) => {
+    const updated = movies.map((m) =>
+      m === movie ? { ...m, cart: !m.cart } : m
+    );
+
+    // Update cart count
+    const newCartCount = updated.filter((m) => m.cart).length;
+
+    setMovies(updated);
+    setCartCount(newCartCount);
+  };
+   // search Functionality
+    const filterred = movies.filter((m)=>m.title.toLocaleLowerCase().includes(search.toLocaleLowerCase()));
+   
+  return (
+    <>
+      <Navbar cartCount={cartCount} onSearch ={(text)=>setSearch(text) }/>
+      <MovieList
+        movies={filterred}
+        addStars={handleIncStar}
+        decStars={handleDecStar}
+        addFav={handleFav}
+        addCart={handleCart}
+      />
+    </>
   );
-  }
-  
-}
+};
 
 export default App;
--- src/navBar.js
+++ src/navBar.js
@@ -1,13 +1,15 @@
 
 import styles from "./navBar.module.css"
 function Navbar(props){
-        const {cartCount} = props
+        const {cartCount, onSearch} = props
+        
+
         return(
            <div className={styles.nav}>
             <div className={styles.title}> <h1>MovieFliex</h1></div>
             <span>
-            <input placeholder="search movies" className={styles.searchInput}/>
-            <button className={styles.searchButton}>search</button>
+            <input type="text" placeholder="search movies" className={styles.searchInput} onChange={(e)=>onSearch(e.target.value)}/>
+            {/* <button className={styles.searchButton} onClick={handileSearch}>search</button> */}
             </span>
             <div className={styles.cartLogo}>
                 <img src="https://cdn-icons-png.flaticon.com/128/4290/4290854.png" alt="Cart Logo" className={styles.image}
